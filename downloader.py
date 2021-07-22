60498a88797d71c0db65a99da12a6e37e53c3c48
ktkk <dezuttereluka@gmail.com>
2021-07-22 22:32:38 +0200

    Rewrote the script to make use of the polyhaven API instead of scraping the website

--- downloader.py
+++ downloader.py
@@ -4,40 +4,33 @@
 
 # Import modules
 import requests
+import json
 import os
 from sys import argv
-#from bs4 import BeautifulSoup
-#from urllib.parse import urlparse
-#from urllib.request import urlretrieve
-#from urllib.request import URLopener
-#from fake_useragent import UserAgent
+from urllib.request import URLopener
 
 # arguments
 name, resolution, category, ext = argv
+# url opener
+opener = URLopener()
 
-print("Resolution: ", resolution)
-print("Category: ", category)
-
-# ua = UserAgent()
-# opener = URLopener()
-# opener.addheader('User-Agent', ua.chrome)
+print("Resolution:", resolution)
+print("Category:", category)
+print("Extention:", ext)
 
 url = 'https://api.polyhaven.com'
 hdris = "/assets?t=hdris"
 files = "/files"
 
-hdri_list = url + hdris
-
+# get the url for the hdri json
+hdri_url = url + hdris
 if category != "all":
-    hdri_list = hdri_list + "&c=" + category
+    hdri_url = hdri_url + "&c=" + category
 
-print(url_category)
+# get a list of all the hdri keys
+hdris = list(requests.get(hdri_url).json().keys())
 
-# r = requests.get(url_category, allow_redirects=True, headers={'User-Agent': ua.chrome})
-# soup = BeautifulSoup(r.text, 'html.parser')
-# print(soup.prettify())
-
-# make a dir for category
+# make a dir for resolution
 save_to = 'hdri_' + resolution
 try:
     os.mkdir(save_to)
@@ -45,37 +38,16 @@ except Exception as e:
     pass
 os.chdir(save_to)
 
-# find the div with the links
-# hdris = soup.find_all("div", class_="Grid_grid__cJ-Hm")
-# print(hdris)
-# 
-# for hdri in hdris:
-#     thumbnail = hdri.select('.thumbnail')[0]['data-src']
-#     href = urlparse(hdri['href'])
-#     filename = href.query[2:] + '_' + resolution
-# 
-#     # DL link example
-#     # https://polyhaven.com/hdris/small_harbor_02_2k.hdr
-#     dl_url = (
-#         'https://polyhaven.com/hdris/' + filename
-#     )
-#     thumbnail_url = 'https://polyhaven.com' + thumbnail
-#     print(dl_url)
-#     print(thumbnail_url)
-# 
-#     try:
-#         print('downloading hdr...')
-#         ext = '.hdr'
-#         opener.retrieve(dl_url + ext, filename + ext)
-#     except Exception as e:
-#         print('hdr download failed, trying exr...')
-#         try:
-#             ext = '.exr'
-#             opener.retrieve(dl_url + ext, filename + ext)
-#         except Exception as e:
-#             print('download failed. Continuing...\n')
-#             continue
-#     print('')
-#     opener.retrieve(thumbnail_url, os.path.basename(thumbnail_url))
+for hdri in hdris:
+    file_json = requests.get(url + files + "/" + hdri).json()
+
+    try:
+        print("url:", file_json["hdri"][resolution][ext]["url"])
+        with open(f'{hdri}.{ext}','wb') as hdri_file:
+            response = requests.get(file_json["hdri"][resolution][ext]["url"], allow_redirects = True)
+            hdri_file.write(response.content)
+    except Exception as e:
+        print("Download failed, possibly because", ext, "is not available for this image.")
+        continue
 
 print('Done')
